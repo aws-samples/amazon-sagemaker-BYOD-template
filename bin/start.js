5cb08f4d18dc4c6cc0d1242354cda953365d26b8
John M Calhoun <johnmcalhoun123@gmail.com>
2019-01-09 15:55:18 -0500

    updated start.js to wait

--- bin/start.js
+++ bin/start.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 var aws=require('../template/bin/aws')
 var sns=new aws.SNS()
+var cloudformation=new aws.CloudFormation()
+var step=new aws.StepFunctions()
 var config=require('../config')
 var GetOutput=require('../template/bin/output').run
 
@@ -13,6 +15,26 @@ GetOutput().then(async output=>{
     }).promise()
     console.log("SageBuild pipeline started succesfully")
     console.log(result)
+    return output
+})
+.then(async output=>{
+    return new Promise(async (res,rej)=>{
+        next()
+        async function next(){
+            var executions=await step.listExecutions({
+                stateMachineArn:output.StateMachine,
+                statusFilter:"RUNNING"
+            }).promise()
+
+            if(executions.executions.length>0){
+                console.log('.')
+                setTimeout(x=>next(),5000)
+            }else{
+                console.log('finished')
+                res()
+            }
+        }
+    })
 })
 .catch(e=>{
     console.log("SageBuild pipeline failed")
